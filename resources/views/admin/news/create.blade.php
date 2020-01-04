9a1001968b3568d41556d7f66efefc57e50ec30a
名前 <>
2020-01-04 09:43:10 +0000

    first-commit

---
+++ resources/views/admin/news/create.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" conten="width=device-width, initial-scale=1">
+        
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>Myニュース作成画面</h1>
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>edit action作成画面</title>
+        
+    </head>
+    <body>
+        <h1>こちらはedit actionです</h1>
+    </body>
+</html>
